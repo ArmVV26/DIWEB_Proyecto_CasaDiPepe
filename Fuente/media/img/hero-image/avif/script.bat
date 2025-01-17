83054391c85bd491f7208a290c03f4de6826e081
ArmVV26 <ditovaquero@gmail.com>
2025-01-17 10:52:53 +0100

    Redimension avif

--- Fuente/media/img/hero-image/avif/script.bat
+++ Fuente/media/img/hero-image/avif/script.bat
@@ -1,5 +1,5 @@
 for %%F in (*) do (    
-    for %%S in (480 768 1200 1920 2560 3200) do (
+    for %%S in (320 640 960 1600 2240 3200) do (
         magick "%%F" -resize %%Sx "%%~nF-%%Spx%%~xF"
         echo Imagen redimensionada: %%~nF-%%Spx%%~xF
     )
