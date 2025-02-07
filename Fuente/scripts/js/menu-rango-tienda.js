5bf05046cf5ac223dd43ff8f04ec761c87f3fc4a
Armando Vaquero Vargas <ditovaquero@gmail.com>
2025-02-07 21:24:36 +0100

    Todos menos Parcel

--- Fuente/scripts/js/menu-rango-tienda.js
+++ Fuente/scripts/js/menu-rango-tienda.js
@@ -1,3 +1,18 @@
+/*
+    En este script voy a hacer los eventos necesarios para:
+        1.- Mostrar/Ocultar el menu secundario de la tienda.
+        2.- Actulizar el boton de rango.
+*/
+
+// Script del menu secundario
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu-desp-sec").addEventListener("click", function() {
+      document.querySelector(".enlaces-secundarios").classList.toggle("activo-sec");
+    });
+  });
+  
+
+
 // Con este codigo muetro el valor que se indica en el range
 document.addEventListener("DOMContentLoaded", function () {
     let valor = document.getElementById("rango-precio");
